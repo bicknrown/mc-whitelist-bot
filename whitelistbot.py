9d93153c4936af857fa85fde94b4c8d13cf88acc
Nick Brown <njbrown4@buffalo.edu>
2021-12-23 23:26:40 -0500

    refactored bot to be more extensible; dropped using application commands; switched to using a config file for settings; switched to using a minecraft RCON wrapper for sending commands.

--- whitelistbot.py
+++ whitelistbot.py
@@ -1,32 +1,57 @@
 import re
 import os
-import subprocess
-import interactions
+import configparser
+import discord
+from discord.ext import commands
+from aiomcrcon import Client
 
-TOKEN = os.getenv("TOKEN")
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-bot = interactions.Client(token=TOKEN)
+# Discord config section.
+TOKEN = config.get("Discord", "TOKEN", fallback="No token found!")
+BOTPREFIX = config.get("Discord", "COMMAND_PREFIX", fallback="*")
+BOTDESCRIPTION = config.get("Discord", "BOT_DESCTIPTION", fallback="A bot to whitelist your Minecraft in-game name!")
+WHITELISTDESCRIPTION = config.get("Discord", "WHITELIST_DESCTIPTION", fallback="A command to whitelist your Minecraft in-game name!")
+
+# Remote minecraft config section.
+
+HOST = config.get("Minecraft", "HOST", fallback="127.0.0.1")
+PORT = int(config.get("Minecraft", "PORT", fallback="25566"))
+PASSWORD = config.get("Minecraft", "PASSWORD", fallback="password")
+
+# Internal config.
+mcNameReMatch = r"^\w{3,16}$"
+success = "You have been added to the whitelist!"
+invalidMcName = "Invalid Username!"
+nameAlreadyAdded = "This username has already been added!"
+nameDNE = "This Username does not exist!"
+whitelistCommand = "whitelist add "
+
+botIntents = discord.Intents.default()
+botIntents.members = True
+
+bot = commands.Bot(command_prefix=BOTPREFIX, description=BOTDESCRIPTION, intents=botIntents)
 
 @bot.event
 async def on_ready():
     print("logged in!")
 
-@bot.command(
-        name="whitelist",
-        description="add a username to the whitelist",
-        options=[interactions.Option(
-            type=interactions.OptionType.STRING,
-            name="ingamename",
-            description="your in-game name",
-            required=True,
-            )]
-        )
-async def whitelist(ctx,ingamename: str):
-    valid = re.match(r"^\w{3,16}$",ingamename)
-    if valid == None:
-        await ctx.send("Invalid Username!")
-    else:
-        subprocess.run(["screen", "-S", "mc", "-X","eval", 'stuff "whitelist add {0}\015"'.format(ingamename)])
-        await ctx.send("You have been added to the whitelist!")
+@bot.command(description=WHITELISTDESCRIPTION)
+async def whitelist(ctx, ign: str):
+    reMatch = re.match(mcNameReMatch,ign)
+    match reMatch:
+        case None:
+            ctx.respond(invalidMcName)
+        case _:
+            client = Client(HOST,PORT,PASSWORD)
+            await client.connect()
+            response = await client.send_cmd(whitelistCommand + ign)
+            await client.close()
+            print(response)
+            ctx.respond(success)
 
-bot.start()
+
+
+
+bot.run(TOKEN)
